69050f7f4ee24394c4e86d286da7715c55f71d39
Kestrel41x4 <kestrel41x4@gmail.com>
2024-09-02 00:58:36 -0500

    project creation

---
+++ data/index.ts
@@ -0,0 +1,194 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I was the head programmer of my high school's robotics team ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/asset_images/frc_logo.png",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "Be sure to checkout my linkedin.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I'm always trying to add to",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "See my projects on Github",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building a game engine using Vulkan",
+      description: "Current Projects",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/asset_images/vulkan_code.png",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Link to my Contact Information",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Diease detection in Plants",
+      des: "Detect diease with the useage of ai",
+      img: "/asset_images/plant_project.jpeg",
+      iconLists: ["/tf.svg", "/python.svg", "/opencv.svg", "/keras.svg"],
+      link: "https://github.com",
+    },
+    {
+      id: 2,
+      title: "Banking App",
+      des: "Using Nextjs to build a banking application.",
+      img: "/",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+      link: "https://github.com",
+    },
+    {
+      id: 3,
+      title: "American Sign Language with YOLO",
+      des: "AI system to detect ASL",
+      img: "/",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+      link: "https://github.com",
+    },
+    {
+      id: 4,
+      title: "Soccer Analysis",
+      des: "detect football score with yolo",
+      img: "/p4.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+      link: "https://github.com",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "asdfawefawefafwfawef",
+      name: "afwaefjoawefawef",
+      title: "afawefawefawefawe",
+    },
+    
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "Tensorflow",
+      img: "/tf.svg",
+      nameImg: "/Tensorflow.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "Pytorch",
+      img: "/pyt.svg",
+      nameImg: "/Pytorch.svg",
+    },
+    {
+      id: 4,
+      name: "OpenCV",
+      img: "/opencv.svg",
+      nameImg: "/OpenCV.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "",
+      desc: "",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "",
+      desc: ".",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "FRC Programmer",
+      desc: ".",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Hacktons",
+      desc: ".",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+    },
+  ];
---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://fd13e61b530407208f3a895e66afdf0e@o4507371693277184.ingest.us.sentry.io/4507371694981120",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
